5e69871ab6833edc293abae9877981e771c40506
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2025-11-25 12:51:18 +0100

    [articles] update model and view for articles

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,12 @@
 from django.db import models
 from django.conf import settings
-from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, blank=True)
+    author = models.CharField(max_length=100, default='Anonymous')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
