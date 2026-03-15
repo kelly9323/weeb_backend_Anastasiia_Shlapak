15d9cf55d39ec2a638d9a57e1eb598ae0ccc8d49
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2026-03-15 15:23:38 +0100

    feat: add CSRF_TRUSTED_ORIGINS setting to production configuration

--- weeb/settings/production.py
+++ weeb/settings/production.py
@@ -10,6 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 # ==============================================================================
 # BASE DE DONNÉES
