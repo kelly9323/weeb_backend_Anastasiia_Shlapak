f7d0102f96f60ace92f15404f684a3ac1cc69afa
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2026-04-28 23:03:21 +0200

    fix: remove redundant CORS_ALLOWED_ORIGINS setting in production configuration

--- weeb/settings/production.py
+++ weeb/settings/production.py
@@ -7,10 +7,10 @@ from .base import *
 # ==============================================================================
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # ==============================================================================
 # BASE DE DONNÉES
 # ==============================================================================
@@ -28,4 +28,3 @@ DATABASES = {
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
